1825683feee4a0f00fb90a6dd1363aedccbb5d1a
OlgaMArsal <inconcretehome@gmail.com>
2020-09-07 15:44:26 +0300

    java_two_lesson_six_170820_HW

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package client;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("client.fxml"));
+        primaryStage.setTitle("ServerChat");
+        primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
     }
 
